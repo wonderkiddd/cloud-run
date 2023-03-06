09dcc16072210a7fe2afbb123c013ea520907865
wonderkiddd <73066195+wonderkiddd@users.noreply.github.com>
2023-03-06 14:55:29 +0700

    asd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('Kayriva For The Win');
 });
 
 const PORT = process.env.PORT || 8080;
